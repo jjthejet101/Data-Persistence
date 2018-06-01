ab27d0ea95ebf88024b5103cde9c03e85dd3c5ef
Unknown <justinkjetton@gmail.com>
2018-06-01 10:00:34 -0500

    assigned db to const var

--- data_persistence_app/routes/index.js
+++ data_persistence_app/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 
+const db = new sqlite3.Database('./Chinook_Sqlite_AutoIncrementPKs.sqlite');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
