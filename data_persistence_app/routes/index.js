f4012089c819c6ea4395a1c7f7aaa4ef4d430f9c
Unknown <justinkjetton@gmail.com>
2018-06-01 10:02:28 -0500

    simple db test

--- data_persistence_app/routes/index.js
+++ data_persistence_app/routes/index.js
@@ -4,6 +4,13 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./Chinook_Sqlite_AutoIncrementPKs.sqlite');
 
+const query = `SELECT * from Artist LIMIT 100`;
+
+db.each(query, (err, row) => {
+  if (err) throw err;
+  console.log(row);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
